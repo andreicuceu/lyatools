337c323f31f7bf495dc0bfc89366808684b6068e
Andrei Cuceu <andreicuceu@gmail.com>
2023-11-23 14:02:40 -0500

    Add other seeds

--- lyatools/scripts/run_vega_fitter.py
+++ lyatools/scripts/run_vega_fitter.py
@@ -34,13 +34,29 @@ def main():
 
     analysis_dir = Path(config['mock_setup'].get('analysis_dir'))
     mock_version = config['mock_setup'].get('mock_version')
-    qq_seeds = config['mock_setup'].get('qq_seeds')
     qq_run_type = config['mock_setup'].get('qq_run_type')
     analysis_name = config['mock_setup'].get('analysis_name', 'baseline')
     output_dir = config['mock_setup'].get('output_dir')
     run_flag = config['mock_setup'].getboolean('run_flag', True)
+    name_extension = config['fit_info'].get('name_extension', None)
+
+    input_seeds = config['mock_setup'].get('input_seeds', None)
+    cat_seeds = config['mock_setup'].get('cat_seeds', None)
+    qq_seeds = config['mock_setup'].get('qq_seeds')
+
+    if input_seeds is None:
+        input_seeds = qq_seeds
+    if cat_seeds is None:
+        cat_seeds = qq_seeds
 
     seeds = np.array(submit_utils.get_seed_list(qq_seeds))
+    input_seeds = submit_utils.get_seed_list(input_seeds)
+    cat_seeds = submit_utils.get_seed_list(cat_seeds)
+
+    if len(input_seeds) != len(seeds):
+        raise ValueError('Number of input seeds and qq seeds must match.')
+    if len(cat_seeds) != len(seeds):
+        raise ValueError('Number of catalog seeds and qq seeds must match.')
 
     num_tasks_per_proc = len(seeds) // num_cpus
     remainder = len(seeds) % num_cpus
@@ -53,9 +69,8 @@ def main():
 
     print_func(f'Rank {cpu_rank} running seeds {start} to {stop}.')
 
-    for seed in seeds[start:stop]:
-        version = f'{mock_version}.{seed}'
-        name_extension = config['fit_info'].get('name_extension', None)
+    for i in range(start, stop):
+        version = f'{mock_version}.{input_seeds[i]}.{cat_seeds[i]}.{seeds[i]}'
         if name_extension is not None:
             config['fit_info']['name_extension'] = f'{name_extension}_{version}'
         else:
