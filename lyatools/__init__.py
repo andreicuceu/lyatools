99b8324f36c4fd85a4c69ccaa65f48019871cde0
andreicuceu <andreicuceu@gmail.com>
2024-11-12 18:14:15 -0500

    Bump version: 1.0.0 → 1.0.1

--- lyatools/__init__.py
+++ lyatools/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Andrei Cuceu"""
 __email__ = 'andreicuceu@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from lyatools.run_one_mock import MockRun
 from lyatools.run_all_mocks import MockBatchRun
