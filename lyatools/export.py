24f6b58eb3905717c796c8302c538f94267ba68a
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2026-09-10 11:28:51 +0200

    updated flag check

--- lyatools/export.py
+++ lyatools/export.py
@@ -497,9 +497,9 @@ def mpi_export(export_commands, export_cov_commands, analysis_tree, job, corr_jo
     individual_cov_commands = []
     smooth_cov_commands = []
     for command in export_cov_commands:
-        if 'write_full_covariance_matrix_flex_size_shuffled' in command:
+        if 'write_full_covariance_matrix' in command:
             individual_cov_commands += [command]
-        elif 'write_smooth_covariance_flex_size' in command:
+        elif 'write_smooth_covariance' in command:
             smooth_cov_commands += [command]
         else:
             raise ValueError(f'Unknown covariance command: {command}')
